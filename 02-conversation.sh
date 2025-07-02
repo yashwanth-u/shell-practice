13ac23e6e9bc221fb5bd98bf25f5c2e2cd939a3c
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-02 06:48:15 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Trump: Hey Musk, H r u?"
+echo "Musk: I'm doing great, Trump! Just busy with SpaceX and Tesla."
+echo "Trump: That's awesome! I just had a rally, it was huge!"
+echo "Musk: I saw that! Your rallies are always a spectacle."
+echo "Trump: Thanks! Speaking of spectacles, have you seen the latest Tesla model?"
+echo "Musk: Yes, it's amazing! The technology is incredible."
