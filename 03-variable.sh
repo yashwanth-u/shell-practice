6a5a6a9805fa4bd43b2de3e1d2ca41920d11aebc
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-02 07:03:18 +0530

    hello world started

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1="Modi"
+PERSON2="Musk"
+
+echo "$PERSON1:: Hi $PERSON2, H r u?"
+echo "$PERSON2:: I'm doing great, Trump! Just busy with SpaceX and Tesla."
+echo "$PERSON1:: That's awesome! I just had a rally, it was huge!"
+echo "$PERSON2:: I saw that! Your rallies are always a spectacle."
+echo "$PERSON1:: Thanks! Speaking of spectacles, have you seen the latest Tesla model?"
+echo "$PERSON2:: Yes, it's amazing! The technology is incredible." 
