5d88c4e8af5fc639383047e21ce20b15e76c9715
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-03 16:08:07 +0530

    special vars

--- 08-special-varsh.sh
+++ 08-special-varsh.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "Current User: $USER"
+echo "Home Directory: $HOME"
+echo "PID of the current script: $$"
+sleep 10&
+echo "PID of the last background process: $!"
